aed48a0a4a24d462012004af71739feb9402b23e
Taylor <taylordavids997@gmail.com>
2023-08-10 17:10:07 +0200

    addcomments of correction

--- Challenge3/js/scripts.js
+++ Challenge3/js/scripts.js
@@ -1,19 +1,19 @@
 const leoName = 'Leo'
-const leoSurname = 'Musvaire     '
+const leoSurname = 'Musvaire'
 const leoBalance = '-9394'
 
-const sarahName = 'Sarah    '
+const sarahName = 'Sarah'                                               // remove space
 const sarahSurname = 'Kleinhans'
 const sarahBalance = '-4582.2'
 
-const divider = '----------------------------------'
+const divider = '\n----------------------------------'                  // \n to put text in the next line
 
 // Only change below this line
 
 const owed = parseInt(leoBalance + sarahBalance)
-const leo = `${leoName} ${leoSurname} Owed R${leoBalance}`
-const sarah = `${sarahName} ${sarahSurname} Owed R${sarahBalance}`
-const total = `Total amount owed: `
-const result = leo + sarah + divider + divider + total + owed + divider
+const leo = `${leoName} ${leoSurname} (Owed R${leoBalance})`              // remove + signs leoSurname leoBalance
+const sarah = `\n${sarahName} ${sarahSurname} (Owed R${sarahBalance})`  // remove + signs Sarahname sarahBalance
+const total = `\n\tTotal amount owed: `                                 // \t add space before text
+const result = leo + sarah + divider + total + owed + divider
 
 console.log(result)
