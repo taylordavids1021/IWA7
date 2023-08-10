8dc5f5b7fc156185195fd3d67e1770997e3f6d77
Taylor <taylordavids997@gmail.com>
2023-08-10 13:56:14 +0200

    add lms code

--- Challenge3/js/scripts.js
+++ Challenge3/js/scripts.js
@@ -1,1 +1,19 @@
-console.log("true")
+const leoName = 'Leo'
+const leoSurname = 'Musvaire     '
+const leoBalance = '-9394'
+
+const sarahName = 'Sarah    '
+const sarahSurname = 'Kleinhans'
+const sarahBalance = '-4582.2'
+
+const divider = '----------------------------------'
+
+// Only change below this line
+
+const owed = parseInt(leoBalance + sarahBalance)
+const leo = `${leoName} ${leoSurname} Owed R${leoBalance}`
+const sarah = `${sarahName} ${sarahSurname} Owed R${sarahBalance}`
+const total = `Total amount owed: `
+const result = leo + sarah + divider + divider + total + owed + divider
+
+console.log(result)
