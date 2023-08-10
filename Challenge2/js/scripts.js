a3837bc97b036070d465689e6490c1905d38f5f3
Taylor <taylordavids997@gmail.com>
2023-08-10 09:43:25 +0200

    add extra const to add both names either or and change ` instead of ""

---
+++ Challenge2/js/scripts.js
@@ -0,0 +1,4 @@
+const nickname = "Timmy";
+const firstname = "Timothy";
+const myName = nickname || firstname        // add both const either or
+console.log(`Good Morning, ${myName}!`)     // add ` instead of "" and one ${ }
