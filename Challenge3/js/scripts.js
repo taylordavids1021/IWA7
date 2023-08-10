578bc09904b301746db4686bc68bc73ef74dca51
Taylor <taylordavids997@gmail.com>
2023-08-10 20:13:15 +0200

    exchange string to number with 2 decimal

--- Challenge3/js/scripts.js
+++ Challenge3/js/scripts.js
@@ -1,19 +1,19 @@
 const leoName = 'Leo'
 const leoSurname = 'Musvaire'
-const leoBalance = '-9394'
+const leoBalance = '9394'
 
 const sarahName = 'Sarah'                                               // remove space
 const sarahSurname = 'Kleinhans'
-const sarahBalance = '-4582.2'
+const sarahBalance = '4582.2'
 
 const divider = '\n----------------------------------'                  // \n to put text in the next line
 
 // Only change below this line
 
-const owed = parseInt(leoBalance + sarahBalance)
-const leo = `${leoName} ${leoSurname} (Owed R${leoBalance})`              // remove + signs leoSurname leoBalance
-const sarah = `\n${sarahName} ${sarahSurname} (Owed R${sarahBalance})`  // remove + signs Sarahname sarahBalance
-const total = `\n\tTotal amount owed: `                                 // \t add space before text
-const result = leo + sarah + divider + total + owed + divider
+const owed = parseInt(leoBalance) + parseFloat(sarahBalance)            // exchange to parseFloat
+const leo = `${leoName} ${leoSurname} (Owed R${parseFloat(leoBalance).toFixed(2)})`              // remove + signs leoSurname leoBalance & exchange string to number with 2 decimal
+const sarah = `\n${sarahName} ${sarahSurname} (Owed R${parseFloat(sarahBalance).toFixed(2)})`  // remove + signs Sarahname sarahBalance & exchange string to number with 2 decimal
+const total = `\n\tTotal amount owed: R${owed.toFixed(2)}`                                 // \t add space before text
+const result = leo + sarah + divider + total + divider
 
 console.log(result)
